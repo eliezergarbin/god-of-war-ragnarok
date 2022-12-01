44ffe92865494fd98c909bdb58a17f149fc04a50
Eliezer Nascimento <eliezergarbin1@gmail.com>
2022-11-30 21:31:25 -0300

    adicionando biblioteca swipper para os slides

---
+++ js/main.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".slide-characters", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  centeredSlides: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
