1efcab6dfae3f23fd428da5035b8bf7e010eb997
Eliezer Nascimento <eliezergarbin1@gmail.com>
2022-11-30 22:03:44 -0300

    concluindo desktop

--- js/main.js
+++ js/main.js
@@ -1,9 +1,5 @@
 var swiper = new Swiper(".slide-characters", {
-  slidesPerView: 4,
-  spaceBetween: 30,
-  centeredSlides: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
+  slidesPerView: 3.5,
+  spaceBetween: 19,
+  freeMode: true,
 });
